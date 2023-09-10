845fd79c5750877fe43934f6b79a0f8da0ae0c8b
SivaSankarr1 <sivasankarr1@outlook.com>
2023-09-10 19:32:27 +0530

    B-works

--- src/org/test/TestingJava.java
+++ src/org/test/TestingJava.java
@@ -3,6 +3,7 @@ package org.test;
 public class TestingJava {
 
 	public static void main(String[] args) {
+		System.out.println("Below are B works");
 		String str = "madam";
 		String rev = "";
 		for (int i = str.length() - 1; i >= 0; i--) {
